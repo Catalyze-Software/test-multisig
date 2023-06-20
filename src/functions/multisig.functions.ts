7a5b87e16f9832e1af209e09941fd7b9b8e72dcc
Remco <remco@r3m.codes>
2023-06-20 15:35:08 +0200

    whitelist actions

--- src/functions/multisig.functions.ts
+++ src/functions/multisig.functions.ts
@@ -101,4 +101,53 @@ export function addToWhitelistTest() {
     await expect(Promise.resolve(result)).resolves.toContain(identities.two().getPrincipal().toString());
     await expect(Promise.resolve(result)).resolves.toContain(identities.three().getPrincipal().toString());
   });
+
+  it("Should say the principal is not whitelisted", async () => {
+    let result = await msTwo.whitelist_request({
+      Remove: identities.four().getPrincipal(),
+    });
+    expect(result).toEqual({ Err: "Principal not whitelisted" });
+  });
+
+  //
+  // IDENTITY FOUR IS THE CALLER FOR THE FOLLOWING METHODS
+  //
+  it("Should say the caller is not whitelisted", async () => {
+    let result = await msFour.whitelist_request({
+      Add: identities.three().getPrincipal(),
+    });
+    expect(result).toEqual({ Err: "Caller is not whitelisted" });
+  });
+
+  it("Should say the caller is not whitelisted", async () => {
+    let result = await msFour.whitelist_request({
+      Remove: identities.three().getPrincipal(),
+    });
+    expect(result).toEqual({ Err: "Caller is not whitelisted" });
+  });
+
+  //
+  // IDENTITY ONE IS THE CALLER FOR THE FOLLOWING METHODS
+  //
+  it("Should say the there is no majority reached", async () => {
+    let result = await msOne.whitelist_request({
+      Remove: identities.two().getPrincipal(),
+    });
+    expect(result).toEqual({ Err: "No marjority reached" });
+  });
+
+  //
+  // IDENTITY THREE IS THE CALLER FOR THE FOLLOWING METHODS
+  //
+  it("Should say the request is approved", async () => {
+    let result = await msThree.vote_on_whitelist_request(3, { Approve: null });
+    expect(result).toEqual({ Ok: "Whitelist request approved" });
+  });
+
+  it("Should have identity one and three in the whitelist", async () => {
+    let result = (await msThree.get_whitelist()).map((p) => p.toString());
+    await expect(Promise.resolve(result)).resolves.toHaveLength(2);
+    await expect(Promise.resolve(result)).resolves.toContain(identities.one().getPrincipal().toString());
+    await expect(Promise.resolve(result)).resolves.toContain(identities.three().getPrincipal().toString());
+  });
 }
